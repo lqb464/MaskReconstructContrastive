3a108147c9cff735cac6837e445356ad1cf8bfb8
Hoang Vo~ <vohoang2204@gmail.com>
2026-02-10 07:52:44 +0700

    feat: comment out error raising for masking option in enforce_recon_only_args function

--- swin_unet/src/ver3/mask_reconstruction/experiment.py
+++ swin_unet/src/ver3/mask_reconstruction/experiment.py
@@ -36,11 +36,11 @@ def enforce_recon_only_args(args: argparse.Namespace) -> None:
             "mask_reconstruction/main.py is reconstruction-only. "
             "Disable contrastive with --disable-contrastive (or remove --enable-contrastive)."
         )
-    if bool(getattr(args, "enable_masking", False)):
-        raise ValueError(
-            "mask_reconstruction/main.py requires masking disabled. "
-            "Use --disable-masking for this task."
-        )
+    # if bool(getattr(args, "enable_masking", False)):
+    #     raise ValueError(
+    #         "mask_reconstruction/main.py requires masking disabled. "
+    #         "Use --disable-masking for this task."
+    #     )
 
 
 __all__ = ["ExperimentConfig", "build_argparser", "enforce_recon_only_args", "UNUSED_IN_RECON_ONLY"]
